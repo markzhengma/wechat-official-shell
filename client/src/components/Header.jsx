471307a3d75f8bd43d34ad8f2d9b0b2a567e3795
Mark Ma <mazheng1022@hotmail.com>
2018-05-28 00:00:20 -0400

    changed logo position

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,7 +10,8 @@ class Header extends Component {
             <header>
                 <div className = "app-header">
                     <div className = "header-logo"/>
-                    <div className = "header-title"><b><small>呼伦贝尔市壳牌{window.innerWidth <= 400 ? <br/> : ''}汽车养护中心</small><br/>保养查询系统</b></div>
+                    <div className = "header-title"><b><small>呼伦贝尔市壳牌{window.innerWidth <= 400 ? <br/> : ''}汽车养护中心</small></b></div>
+                    <div className = "header-title"><b>保养查询系统</b></div>
                 </div>
             </header>
         )
