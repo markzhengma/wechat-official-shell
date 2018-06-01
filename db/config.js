53ab5f5c14e24601773c3724f64b8c04136294a6
Mark Ma <mazheng1022@hotmail.com>
2018-06-01 10:03:21 -0400

    modify config.js for heroku deployment

--- db/config.js
+++ db/config.js
@@ -6,32 +6,32 @@ const options = {
 
 const pgp=require('pg-promise')(options);
 
-function setDatabase(){
-    if(process.env.NODE_ENV==='development'||!process.env.NODE_ENV){
-        return pgp({
-            database: 'wechat_shell_db',
-            port: 5432,
-            host: 'localhost',
-        })
-    } else {
-        return pgp(process.env.DATABASE_URL);
-    }
-}
-
-const db = setDatabase();
-
-module.exports = db;
-
-// let db;
-
-// if (process.env.NODE_ENV==='development'||!process.env.NODE_ENV){
-//     db = pgp({
-//         database: 'wechat_shell_db',
-//         port: 5432,
-//         host: 'localhost',
-//     })
-// } else if (process.env.NODE_ENV === 'production'){
-//     db = pgp(process.env.DATABASE_URL);
+// function setDatabase(){
+//     if(process.env.NODE_ENV==='development'||!process.env.NODE_ENV){
+//         return pgp({
+//             database: 'wechat_shell_db',
+//             port: 5432,
+//             host: 'localhost',
+//         })
+//     } else {
+//         return pgp(process.env.DATABASE_URL);
+//     }
 // }
+
+// const db = setDatabase();
+
+// module.exports = db;
+
+let db;
+
+if (process.env.NODE_ENV==='development'||!process.env.NODE_ENV){
+    db = pgp({
+        database: 'wechat_shell_db',
+        port: 5432,
+        host: 'localhost',
+    })
+} else if (process.env.NODE_ENV === 'production'){
+    db = pgp(process.env.DATABASE_URL);
+}
 
 module.exports = db;
