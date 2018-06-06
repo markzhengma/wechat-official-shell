a685ab171f303208ed7c48722ece7dd23dfe638c
Mark Ma <mazheng1022@hotmail.com>
2018-06-06 09:06:58 -0400

    try to manually set ssl true

--- db/config.js
+++ db/config.js
@@ -1,11 +1,12 @@
-const options = {
-    query:(e) => {
-        console.log(e.query);
-    }
-};
+// const options = {
+//     query:(e) => {
+//         console.log(e.query);
+//     }
+// };
 
-const pgp=require('pg-promise')(options);
+// const pgp=require('pg-promise')(options);
 
+//----------native dev section-------------
 // function setDatabase(){
 //     if(process.env.NODE_ENV==='development'||!process.env.NODE_ENV){
 //         return pgp({
@@ -21,17 +22,35 @@ const pgp=require('pg-promise')(options);
 // const db = setDatabase();
 
 // module.exports = db;
+//-----------native dev section------------//
 
-let db;
+// let db;
 
-if (process.env.NODE_ENV==='development'||!process.env.NODE_ENV){
-    db = pgp({
-        database: 'wechat_shell_db',
-        port: 5432,
-        host: 'localhost',
-    })
-} else if (process.env.NODE_ENV === 'production'){
-    db = pgp(process.env.DATABASE_URL);
-}
+// if (process.env.NODE_ENV==='development'||!process.env.NODE_ENV){
+//     db = pgp({
+//         database: 'wechat_shell_db',
+//         port: 5432,
+//         host: 'localhost',
+//     })
+// } else if (process.env.NODE_ENV === 'production'){
+//     db = pgp(process.env.DATABASE_URL);
+// }
 
-module.exports = db;
+// module.exports = db;
+
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM infromation_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for(let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+})
