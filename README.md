0abb942d1a1bdb8d9ff8142fdbfcb881cfe68d01
Mark Ma <mazheng1022@hotmail.com>
2018-06-21 23:48:27 -0400

    add read me. add more phone numbers

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# wechat-official-shell
-WeChat official account app CRUD functionalities.
+# WeChat Official Shell
+A CRUD app built for Shell car care center's WeChat official account.
+
+## Functionalities
+* Customers search for car care records using plate and phone combination
+* Admin create, update & delete customers and records.
+* Admin search for users & records using service number/plate/phone
+* Generate new service numbers depending on the store when creating new customer info.
+* Browse records using dates and store locations. Download records as csv files.
+
+## Technologies
+* ReactJS
+* NodeJS
+* PostgreSQL
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,11 @@ class Footer extends Component {
         return (
             <footer>
                 <div className = "app-footer">
-                    <a href = 'tel:+86-0470-8223779'>联系电话：0470-8223779</a>
+                    <a href = 'tel:+86-0470-8223779'>海拉尔河东：0470-8223779</a>
+                    <a href = 'tel:+86-0470-8307711'>海拉尔河西：0470-8307711</a>
+                    <a href = 'tel:+86-0470-2205900'>满洲里四道街：0470-2205900</a>
+                    <a href = 'tel:+86-0470-6221541'>满洲里粮库综合楼：0470-6221541</a>
+                    <a href = 'tel:+86-0470-7379457'>牙克石光明南路：0470-7379457</a>
                 </div>
                 <div>
                     {this.props.auth ? 
