dd2f88fe4e24dbfa2b54759715a2c3899a6d2427
Mark Ma <mazheng1022@hotmail.com>
2018-07-04 23:04:22 -0400

    add description to map page

--- client/src/components/MapPage.jsx
+++ client/src/components/MapPage.jsx
@@ -7,12 +7,14 @@ class MapPage extends Component {
         super();
         this.state = {
             location: '',
+            name: '',
             isShowList: true,
         }
     }
-    setLocation = (location) => {
+    setLocation = (location, name) => {
         this.setState({
             location: location,
+            name: name,
             isShowList: !this.state.isShowList,
         })
     }
@@ -24,22 +26,58 @@ class MapPage extends Component {
     render(){
         return (
             <div className = "map-page">
+            <script type="text/javascript" src="https://api.map.baidu.com/api?v=2.0&ak=ygiWYb4B3RVLXFgkNGDZLrYV0jKawnH1"></script>
                 <div className = "location-btn"  onClick = {this.switchListShow}>
                     <b>门店列表</b>
                 </div>
                 <div className = "location-list" style = {this.state.isShowList ? {maxHeight: "600px"}: {maxHeight: "0px"}}>
                     <b>海拉尔</b>
-                    <p className = "location-list-single" onClick = {() => this.setLocation({lng: 119.784342, lat: 49.225553})}>海拉尔河东店</p>
-                    <p className = "location-list-single" onClick = {() => this.setLocation({lng: 119.755715, lat: 49.23229})}>海拉尔河西店</p>
+                    <p className = "location-list-single" onClick = {() => this.setLocation({lng: 119.784342, lat: 49.225553}, "海拉尔河东店")}>海拉尔河东店</p>
+                    <p className = "location-list-single" onClick = {() => this.setLocation({lng: 119.755715, lat: 49.23229}, "海拉尔河西店")}>海拉尔河西店</p>
                     <b>满洲里</b>
-                    <p className = "location-list-single" onClick = {() => this.setLocation({lng: 117.449949, lat: 49.599737})}>满洲里四道街店</p>
-                    <p className = "location-list-single" onClick = {() => this.setLocation({lng: 117.503674, lat: 49.58917})}>满洲里粮库综合楼店</p>
+                    <p className = "location-list-single" onClick = {() => this.setLocation({lng: 117.449949, lat: 49.599737}, "满洲里四道街店")}>满洲里四道街店</p>
+                    <p className = "location-list-single" onClick = {() => this.setLocation({lng: 117.503674, lat: 49.58917}, "满洲里粮库综合楼店")}>满洲里粮库综合楼店</p>
                     <b>牙克石</b>
-                    <p className = "location-list-single" onClick = {() => this.setLocation({lng: 120.718067, lat: 49.285365})}>牙克石光明南路店</p>
+                    <p className = "location-list-single" onClick = {() => this.setLocation({lng: 120.718067, lat: 49.285365}, "牙克石光明南路店")}>牙克石光明南路店</p>
                 </div>
+                {this.state.name === "海拉尔河东店" ? 
+                    <div className = "location-detail" style = {this.state.isShowList ? {opacity: 0} : {opacity: 1}}>
+                        <b>海拉尔河东店</b>
+                        <a href = 'tel:+86-0470-8223779'>联系电话：0470-8223779</a>
+                        <p>地址：海拉尔河东加格达奇路109综合楼1号门市</p>
+                    </div>
+                : ""}
+                {this.state.name === "海拉尔河西店" ? 
+                    <div className = "location-detail" style = {this.state.isShowList ? {opacity: 0} : {opacity: 1}}>
+                        <b>海拉尔河西店</b>
+                        <a href = 'tel:+86-0470-8307711'>联系电话：0470-8307711</a>
+                        <p>地址：海拉尔河西碧海金城夹信子二道街路口</p>
+                    </div>
+                : ""}
+                {this.state.name === "满洲里四道街店" ? 
+                    <div className = "location-detail" style = {this.state.isShowList ? {opacity: 0} : {opacity: 1}}>
+                        <b>满洲里四道街店</b>
+                        <a href = 'tel:+86-0470-2205900'>联系电话：0470-2205900</a>
+                        <p>地址：满洲里四道街西头车之美宾馆西侧门市</p>
+                    </div>
+                : ""}
+                {this.state.name === "满洲里粮库综合楼店" ? 
+                    <div className = "location-detail" style = {this.state.isShowList ? {opacity: 0} : {opacity: 1}}>
+                        <b>满洲里粮库综合楼店</b>
+                        <a href = 'tel:+86-0470-6221541'>联系电话：0470-6221541</a>
+                        <p>地址：满洲里粮库综合楼5号门市</p>
+                    </div>
+                : ""}
+                {this.state.name === "牙克石光明南路店" ? 
+                    <div className = "location-detail" style = {this.state.isShowList ? {opacity: 0} : {opacity: 1}}>
+                        <b>牙克石光明南路店</b>
+                        <a href = 'tel:+86-0470-7379457'>联系电话：0470-7379457</a>
+                        <p>地址：牙克石光明南路民生B区26号楼9号门市</p>
+                    </div>
+                : ""}
                 {this.state.location ? 
                     <div className = "map-container">
-                        <Map center={this.state.location} zoom="17">
+                        <Map center={this.state.location} zoom="17" style={{height: "100%", width: "100%"}}>
                             <Marker position={this.state.location} />
                             <NavigationControl /> 
                             {/* <InfoWindow position={this.state.location} text="内容" title="标题"/> */}
